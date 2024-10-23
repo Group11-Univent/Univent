bbff496a17e2808d9f2b7a92963747292725963f
Abaoyowa Amoye <aamoy013@uottawa.ca>
2024-10-22 20:43:15 -0400

    edited 'product-features.html' and fixed bug in 'home.html'

---
+++ main.js
@@ -0,0 +1,7 @@
+const feature_titles = ["Placeholder1","Placeholder2","Placeholder2"];
+
+let current_feature_title = feature_titles[0];
+
+const feature_desc = ["Placeholder1","Placeholder2","Placeholder2"];
+
+let current_feature_desc = feature_desc[0];
